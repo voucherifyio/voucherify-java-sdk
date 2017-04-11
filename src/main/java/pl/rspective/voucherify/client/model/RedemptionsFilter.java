38953c4d4c342aff3a631a39783c540d09e0c12a
Łukasz Wiktor <lukasz.wiktor@gmail.com>
2017-04-11 12:13:20 +0200

    fix redemptions filter
    - drop no-longer supported start_date and end_date params
    - filter by single result only
    - add filter by campaign

--- src/main/java/pl/rspective/voucherify/client/model/RedemptionsFilter.java
+++ src/main/java/pl/rspective/voucherify/client/model/RedemptionsFilter.java
@@ -1,33 +1,25 @@
 package pl.rspective.voucherify.client.model;
 
-import java.text.SimpleDateFormat;
 import java.util.AbstractMap;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import pl.rspective.voucherify.client.Constants;
 
 public class RedemptionsFilter extends AbstractMap<String, Object> {
 
     private final Integer limit;
-    private final Date startDate;
-    private final Date endDate;
-    private final List<RedemptionResult> results;
+    private final Integer page;
+    private final RedemptionResult result;
     private final String customer;
+    private final String campaign;
     
-    private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(Constants.ENDPOINT_DATE_FORMAT);
-    
-    private RedemptionsFilter(Integer limit, Date startDate, Date endDate, List<RedemptionResult> results, String customer) {
+    private RedemptionsFilter(Integer limit, Integer page, RedemptionResult result, String customer, String campaign) {
         this.limit = limit;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.results = results;
+        this.page = page;
+        this.result = result;
         this.customer = customer;
+        this.campaign = campaign;
     }
     
     public static RedemptionsFilter filter() {
@@ -35,33 +27,25 @@ public class RedemptionsFilter extends AbstractMap<String, Object> {
     }
     
     public RedemptionsFilter withLimit(Integer limit) {
-        return new RedemptionsFilter(limit, startDate, endDate, results, customer);
+        return new RedemptionsFilter(limit, page, result, customer, campaign);
     }
     
-    public RedemptionsFilter withStartDate(Date startDate) {
-        return new RedemptionsFilter(limit, startDate, endDate, results, customer);
+    public RedemptionsFilter withPage(Integer page) {
+        return new RedemptionsFilter(limit, page, result, customer, campaign);
     }
-    
-    public RedemptionsFilter withEndDate(Date endDate) {
-        return new RedemptionsFilter(limit, startDate, endDate, results, customer);
+
+    public RedemptionsFilter withResult(RedemptionResult result) {
+        return new RedemptionsFilter(limit, page, result, customer, campaign);
     }
-    
-    public RedemptionsFilter withResult(RedemptionResult ... results) {
-        return new RedemptionsFilter(limit, startDate, endDate, Arrays.asList(results), customer);
-    }
-    
+
     public RedemptionsFilter withCustomer(String customer) {
-        return new RedemptionsFilter(limit, startDate, endDate, results, customer);
+        return new RedemptionsFilter(limit, page, result, customer, campaign);
     }
     
-    private String formatDate(Date date) {
-        if (date != null) {
-            return DATE_FORMATTER.format(date);
-        } else {
-            return null;
-        }
+    public RedemptionsFilter withCampaign(String campaign) {
+        return new RedemptionsFilter(limit, page, result, customer, campaign);
     }
-    
+        
     private static Map.Entry<String, Object> newEntry(String key, Object value) {
         return new AbstractMap.SimpleEntry<>(key, value);
     }
@@ -70,14 +54,10 @@ public class RedemptionsFilter extends AbstractMap<String, Object> {
     public Set<Map.Entry<String, Object>> entrySet() {
         Set<Map.Entry<String, Object>> entries = new HashSet<>();
         entries.add(newEntry("limit", limit));
-        entries.add(newEntry("start_date", formatDate(startDate)));
-        entries.add(newEntry("end_date", formatDate(endDate)));
-        if (results != null) {
-            for (RedemptionResult result: results) {
-                entries.add(newEntry("result", result));
-            }
-        }
+        entries.add(newEntry("page", page));
+        entries.add(newEntry("result", result));
         entries.add(newEntry("customer", customer));
+        entries.add(newEntry("campaign", campaign));
         return Collections.unmodifiableSet(entries);
     }
 
