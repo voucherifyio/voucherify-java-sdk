ebfaf14b6373832597dc760d36168196cadb405f
Łukasz Kulig <lukasz.kulig88@gmail.com>
2017-08-21 10:33:41 +0200

    Metadata in VoucherValidation

--- src/main/java/io/voucherify/client/model/validation/VoucherValidation.java
+++ src/main/java/io/voucherify/client/model/validation/VoucherValidation.java
@@ -5,9 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Singular;
 import lombok.ToString;
 import io.voucherify.client.model.customer.Customer;
 import io.voucherify.client.model.order.Order;
+
+import java.util.Map;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -19,4 +22,7 @@ public class VoucherValidation {
   private Customer customer;
 
   private Order order;
+
+  @Singular("metadataEntry")
+  private Map<String, Object> metadata;
 }
