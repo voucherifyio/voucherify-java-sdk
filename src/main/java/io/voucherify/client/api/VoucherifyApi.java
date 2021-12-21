34cc730863ef5a68ad6e3d46d2e9162c7cbe3f01
julien.fougere <julien.fougere@transatel.net>
2021-12-22 00:17:44 +0800

    Fix call to release session API

--- src/main/java/io/voucherify/client/api/VoucherifyApi.java
+++ src/main/java/io/voucherify/client/api/VoucherifyApi.java
@@ -235,8 +235,8 @@ public interface VoucherifyApi {
   Call<QualificationList<VoucherResponse>> getQualifiedVouchers(
       @Body QualificationContext validationContext, @QueryMap Map<String, Object> params);
 
-  @DELETE("vouchers/{code}/sessions/{session_key")
-  Call<Void> releaseSession(@Path("code") String code, @Query("session_key") String sessionKey);
+  @DELETE("vouchers/{code}/sessions/{session_key}")
+  Call<Void> releaseSession(@Path("code") String code, @Path("session_key") String sessionKey);
 
   // VALIDATIONS
 
