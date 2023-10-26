23608b8bbd4cfe70a5f2a69f03c81b0abd861121
jkaliszuk <jakub.kaliszuk@rspective.pl>
2023-10-26 11:14:10 +0200

    Added missing field in RedeemableDataResponse.

--- src/main/java/io/voucherify/client/model/qualifications/response/RedeemableDataResponse.java
+++ src/main/java/io/voucherify/client/model/qualifications/response/RedeemableDataResponse.java
@@ -1,16 +1,35 @@
 package io.voucherify.client.model.qualifications.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.voucherify.client.model.common.response.applicableTo.ApplicableToResponse;
+import io.voucherify.client.model.common.response.applicableTo.InapplicableToResponse;
+import io.voucherify.client.model.stackable.response.RedeemableResultResponse;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-//@NoArgsConstructor(access = AccessLevel.PRIVATE)
-//@AllArgsConstructor(access = AccessLevel.PRIVATE)
-//@Getter
-//@ToString
+import java.util.Date;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@ToString
 public class RedeemableDataResponse {
-  //TODO ADD FIELDS AFTER STACKABLE DISCOUNT MERGED
+
+  private String id;
+
+  private String object;
+
+  @JsonProperty("created_at")
+  private Date createdAt;
+
+  @JsonProperty("applicable_to")
+  private ApplicableToResponse applicableTo;
+
+  @JsonProperty("inapplicable_to")
+  private InapplicableToResponse inapplicableTo;
+
+  private RedeemableResultResponse result;
 }
