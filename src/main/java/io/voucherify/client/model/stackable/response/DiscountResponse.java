e30c43d5ec901762ada622ed2f789e14ca3aae73
jkaliszuk <jakub.kaliszuk@rspective.pl>
2024-03-12 11:14:37 +0100

    11.3.0 - adding missing fields to `DiscountResponse`. According to current state of docs.

--- src/main/java/io/voucherify/client/model/stackable/response/DiscountResponse.java
+++ src/main/java/io/voucherify/client/model/stackable/response/DiscountResponse.java
@@ -1,6 +1,9 @@
 package io.voucherify.client.model.stackable.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.voucherify.client.model.product.Product;
+import io.voucherify.client.model.product.SKU;
+import io.voucherify.client.model.voucher.DiscountEffect;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,10 +17,46 @@ import lombok.ToString;
 public class DiscountResponse {
 
   private String type;
-  private String effect;
+
+  @JsonProperty("effect")
+  private DiscountEffect effect;
 
   @JsonProperty("amount_off")
   private Long amountOff;
+
+  @JsonProperty("percent_off")
+  private Double percentOff;
+
+  @JsonProperty("fixed_amount")
+  private Double fixedAmount;
+
+  @JsonProperty("fixed_amount_formula")
+  private String fixedAmountFormula;
+
+  @JsonProperty("percent_off_formula")
+  private String percentOffFormula;
+
+  @JsonProperty("amount_limit")
+  private Double amountLimit;
+
+  @JsonProperty("amount_off_formula")
+  private String amountOffFormula;
+
+  @JsonProperty("aggregated_amount_limit")
+  private Long aggregatedAmountLimit;
+
+  @JsonProperty("unit_off")
+  private Long unitOff;
+
+  @JsonProperty("unit_off_formula")
+  private String unitOffFormula;
+
+  @JsonProperty("unit_type")
+  private String unitType;
+
+  private Product product;
+
+  private SKU sku;
 
   @JsonProperty("is_dynamic")
   private Boolean isDynamic;
